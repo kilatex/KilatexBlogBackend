09a84c853a13e983dfa08ef1367da92ee188cd54
Santiago <santiagodmaldon@gmail.com>
2022-02-21 19:43:02 -0400

    Creating Database

---
+++ database/database.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE IF NOT EXISTS api_rest_laravel;
+
+USE api_rest_laravel;
+
+CREATE TABLE IF NOT EXISTS users(
+id int(255) auto_increment not null,
+name        varchar(50) NOT NULL,
+surname     varchar(100) NOT NULL,
+role        varchar(20),
+email       varchar(255) NOT NULL,
+username    varchar(255) NOT NULL,
+password    varchar(255) NOT NULL,
+description text,
+image       varchar(255),
+created_at  datetime,
+updated_at  datetime,
+remember_token  varchar(255),
+CONSTRAINT pk_users PRIMARY KEY(id)
+)ENGINE=InnoDb;
+
+CREATE TABLE IF NOT EXISTS  categories(
+id int(255) auto_increment not null,
+name        varchar(100) NOT NULL,
+created_at  datetime,
+updated_at  datetime,
+CONSTRAINT pk_categories PRIMARY KEY(id)
+)ENGINE=InnoDb;
+
+CREATE TABLE IF NOT EXISTS  posts(
+id int(255) auto_increment not null,
+user_id int(255) not null,
+category_id int(255) not null,
+title varchar(255) not null,
+content text not null,
+image varchar(255) not null,
+created_at  datetime,
+updated_at  datetime,
+CONSTRAINT pk_posts PRIMARY KEY(id),
+CONSTRAINT fk_posts_user FOREIGN KEY(user_id) REFERENCES users(id),
+CONSTRAINT fk_posts_category FOREIGN KEY(category_id) REFERENCES categories(id)
+)ENGINE=InnoDb;
