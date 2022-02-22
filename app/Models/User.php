a0f4207413a079bff723e4d3902e6fdd0d6bc52b
Santiago <santiagodmaldon@gmail.com>
2022-02-21 20:32:36 -0400

    Creating Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,13 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
+        'role',
         'email',
+        'username',
         'password',
+        'description',
+        'image'
     ];
 
     /**
